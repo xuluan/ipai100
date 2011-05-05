ad027929fef9fbf0fb5ace8c8c32d038d8afe573
Ubuntu <ubuntu@ip-10-196-191-54.ec2.internal>
2011-05-05 16:32:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipai100::Application.config.secret_token = '5f991a0843a19b3daf7fa67205153d1b01f1e59269678d6338213b60ca2aa82e7a3427b2e5c6e72d942247b9294e96b66e5058aa9b492a263bd31433dfd9884b'
